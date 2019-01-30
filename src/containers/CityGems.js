f542651fe9d0969d89de8b280d5e5f197d5eb3d6
github username <github email address>
2019-01-30 15:56:10 -0500

    first commit - set up folders

---
+++ src/containers/CityGems.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+
+export default class CityGems extends Component {
+
+
+  render(){
+    return()
+  }
+}
--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import combineReducers from './reducers/index'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(
+  combineReducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

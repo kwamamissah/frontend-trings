6b7a93464c362eccde27bb2dab33cc8d3ab3293a
github username <github email address>
2019-02-04 12:04:37 -0500

    got weather to show in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Menu } from 'semantic-ui-react'
+import { Icon, Image, Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
 let city = 'Atlanta'
@@ -9,20 +9,41 @@ const WeatherAPI = `api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${AP
 export default class Navbar extends Component {
   state = {
     activeItem: 'home',
-    weather: ''
+    weather: []
   }
 
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
   fetchWeather = (city = 'Atlanta') => {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=5bbb1b66f4a5c0d2731ed0a9d297cb63`)
+    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=5bbb1b66f4a5c0d2731ed0a9d297cb63&units=imperial`)
     .then(resp => resp.json())
-    .then(console.log)
+    .then(weather => this.setState({weather: weather}))
   }
+
+
 
   componentDidMount(){
     this.fetchWeather()
   }
+
+  getTemp = () => {
+    let main = this.state.weather.main
+    let name = this.state.weather.name
+    let weather = this.state.weather.weather
+
+    if (main === undefined) {
+      console.log('content is loading')
+    } else {
+      let temp = main.temp
+      let icon = weather[0].icon
+      let description = weather[0].description
+
+      console.log(name, <img src = {`http://openweathermap.org/img/w/${icon}.png`} alt="icon"/>, Math.round(temp))
+      return (name, Math.round(temp))
+
+    }
+  }
+
 
   render() {
     const { activeItem } = this.state
@@ -47,12 +68,25 @@ export default class Navbar extends Component {
             onClick={this.handleItemClick}/>
         </Link>
 
+        <Menu.Item
+          name={this.state.weather.name}/>
+
+        <Menu.Item
+         name='weather icon'>
+         <Image src='http://openweathermap.org/img/w/04d.png' as={'i'} />
+       </Menu.Item>
+
+        <Menu.Item
+          name={this.getTemp()}/>
+
         <Menu.Menu position='right'>
           <Menu.Item
             name='login'
             active={activeItem === 'login'}
             onClick={this.handleItemClick}/>
           </Menu.Menu>
+
+
       </Menu>
     )
   }

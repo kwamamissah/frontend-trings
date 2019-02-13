9c253c430b4f46851027bd50007b2628286252f4
github username <github email address>
2019-02-13 14:26:32 -0500

    favs load on profile load but can't do a hard refresh or page breaks

--- src/reducers/profile.js
+++ src/reducers/profile.js
@@ -6,7 +6,9 @@ const initial = {
 export default function(state = initial, action){
   switch(action.type){
     case 'FETCH_USER':
-      return {...state, user: action.user};
+      let self = action.user.find(i => i.id === parseInt(localStorage.id))
+      let likedGems = self.likes.map(i => i.city_gem)
+      return {...state, user: action.user, favs: likedGems.reverse()};
     case 'FAVORITE_GEMS':
       let me = state.user.find(i => i.id === parseInt(action.userId))
       let liked = me.likes.map(i => i.city_gem)
